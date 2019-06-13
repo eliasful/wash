efdc60752e0675bc3d1376e7333c51e6173460c5
Elias <elias.vieira@softfocus.com.br>
2019-06-13 13:32:29 -0300

    implementa metodo de salvar e enviar para a tela de serviços

--- app/pods/components/form/form-professional-services/component.js
+++ app/pods/components/form/form-professional-services/component.js
@@ -3,9 +3,11 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   store: service(),
+  router: service(),
   serviceType: null,
   servicesType: null,
   userServices: null,
+  userService: null,
   async didInsertElement() {
     const servicesType = await this.get('store').findAll('service-type');
     this.set('servicesType', servicesType);
@@ -13,14 +15,21 @@ export default Component.extend({
   },
   newUserService() {
     const userService = this.get('store').createRecord('user-service');
+    userService.set('user', this.get('model'));
     this.set('userService', userService);
   },
   actions: {
-    addService() {
+    async addService() {
       const userService = this.get('userService');
+      const price = parseFloat(userService.get('price'));
+      userService.set('price', price);
+      await userService.save();
       this.get('model.userServices').pushObject(userService);
       this.get('model').save();
       this.newUserService();
+    },
+    nextRoute() {
+      this.get('router').transitionTo('professional.new.address', this.get('model'))
     }
   }
 });
