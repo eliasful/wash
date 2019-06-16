6d1dcf8d1bd4fba9dd5b610a967001c00b81d5b6
Elias <eliasful@gmail.com>
2019-06-16 03:43:22 -0300

    cria dashboard usuario

--- app/models/user-solicitation.js
+++ app/models/user-solicitation.js
@@ -1,8 +1,19 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default DS.Model.extend({
   owner: DS.belongsTo('user'),
   userService: DS.belongsTo('user-service'),
   observation: DS.attr('string'),
-  quantity: DS.attr('number')
+  quantity: DS.attr('number'),
+  status: DS.attr('string'),
+  estimatedTime: DS.attr('date'),
+  acceptedIn: DS.attr('date'),
+
+  timeLeft: computed('estimatedTime', 'acceptedIn', function () {
+    const estimatedTime = moment(this.get('estimatedTime'));
+    const acceptedIn = moment(this.get('acceptedIn'));
+    return estimatedTime.diff(acceptedIn, 'H');
+  })
 });
