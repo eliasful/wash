5ff281ffa47794a4158f6d0fa840787dcccae5fd
Elias <eliasful@gmail.com>
2019-06-19 23:57:19 -0300

    correção no find

--- app/pods/professional/dashboard/solicitations/route.js
+++ app/pods/professional/dashboard/solicitations/route.js
@@ -2,10 +2,11 @@ import Route from '@ember/routing/route';
 import {A} from '@ember/array';
 
 export default Route.extend({
-  async model(params) {
+  async model(params, transition) {
     await this.store.findAll('user-solicitation');
+    const user = transition.params["professional.dashboard"].user_id;
     const userServices = await this.store.query('user-service', {
-      user: params.user_id
+      user
     });
     const userSolicitations = A([]);
     userServices.forEach(userService => {
